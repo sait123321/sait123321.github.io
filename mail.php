005096c33ffa80d024744eb99bd035a276d4fcbf
sait123321 <57044781+sait123321@users.noreply.github.com>
2019-10-27 22:42:48 +0700

    Add files via upload

--- mail.php
+++ mail.php
@@ -1,13 +1,43 @@
- <?php
-$to = "temic1997@bk.ru"; // емайл получателя данных из формы 
-$tema = "Форма обратной связи на PHP"; // тема полученного емайла 
-$message = "Ваше имя: ".$_POST['name']."<br>";//присвоить переменной значение, полученное из формы name=name
-$message1 = "Ваша фамилия: ".$_POST['fam']."<br>";//присвоить переменной значение, полученное из формы name=name
-$message2 = "Компания: ".$_POST['comp']."<br>";//присвоить переменной значение, полученное из формы name=name
-  $message3 .= "E-mail: ".$_POST['email']."<br>"; //полученное из формы name=email
-$message4 .= "Номер телефона: ".$_POST['phone']."<br>"; //полученное из формы name=phone
-$message5 .= "Сообщение: ".$_POST['message']."<br>"; //полученное из формы name=message
-$headers6  = 'MIME-Version: 1.0' . "\r\n"; // заголовок соответствует формату плюс символ перевода строки
-  $headers .= 'Content-type: text/html; charset=utf-8' . "\r\n"; // указывает на тип посылаемого контента
-mail($to, $tema,$message, $message1,$message2,$message3,$message4,$message5,$message6, $headers); //отправляет получателю на емайл значения переменных
+<?php
+$spam = $_POST['spam'];
+$msg_box = "";
+if($_POST['btn_submit']){
+$errors = array();
+if($_POST['name'] == "") $errors[] = "Поле 'Ваше имя' не заполнено!";
+if($_POST['sname'] == "") $errors[] = "Поле 'Ваша фамилия' не заполнено!";
+if($_POST['comp'] == "") $errors[] = "Поле 'Компания' не заполнено!";
+if($_POST['email'] == "") $errors[] = "Поле 'Почта' не заполнено!";
+if($_POST['phone'] == "") $errors[] = "Поле 'Телефон' не заполнено!";
+if($_POST['message'] == "") $errors[] = "Поле 'Текст сообщения' не заполнено!";
+if(empty($errors)){
+$message .= "имя: ".$_POST['name']."<br>";
+$message .= "фамилия: ".$_POST['sname']."<br>";
+$message .= "отчество: ".$_POST['otch']."<br>";
+$message .= "Компания: ".$_POST['comp']."<br>";
+$message .= "E-mail: ".$_POST['email']."<br>";
+$message .= "Номер телефона: ".$_POST['phone']."<br>"; 
+$message .= "Сообщение: ".$_POST['message']."<br>"; 
+$message .= "время: ".$_POST['sel']."<br>"; 
+send_mail($message); 
+$msg_box = "<span style='color: green;'>Сообщение успешно отправлено!</span>";
+}
+else
+{
+$msg_box = "";
+foreach($errors as $one_error){
+$msg_box .= "<span style='color: red;'>$one_error</span><br/>";
+}
+}
+}
+function send_mail($message){
+$mail_to = "temic1997@bk.ru";
+$subject = "Письмо с обратной связи";
+$headers= "MIME-Version: 1.0\r\n";
+$headers .= "Content-type: text/html; charset=utf-8\r\n"; // кодировка письма
+$headers .= "From: Тестовое письмо <адрес_почты_на_хосте>\r\n"; // от кого письмо
+if (empty($spam)){ 
+mail($mail_to, $subject, $message, $headers);
+  header("Location: index.html");
+  } else exit ;
+}
 ?>
